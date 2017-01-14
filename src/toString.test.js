4aa448b7a1536b505063c66335bca5adebe27dd0
Nicolas Jakob <nicow.jakob@gmail.com>
2017-01-14 01:24:09 +0100

    Add more tests for toString

--- src/toString.test.js
+++ src/toString.test.js
@@ -1,7 +1,19 @@
 import test from 'ava';
+import Bugsy from './Bugsy';
+import * as syslog from './syslog';
 import toString from './toString';
 
-test('stringify native Error', (t) => {
+test('native error', (t) => {
   t.is(toString(new Error()), '[err] Error');
+  t.pass();
+});
+
+test('severity', (t) => {
+  t.is(toString(new Bugsy({ severity: syslog.ALERT })), '[alert] Error');
+  t.pass();
+});
+
+test('code', (t) => {
+  t.is(toString(new Bugsy({ code: 'performance' })), '[err] performance');
   t.pass();
 });
