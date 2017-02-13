9d597ddcd62fdd1ef5f203c6e0dd80237a6b3a09
Nicolas Jakob <nicow.jakob@gmail.com>
2017-02-13 20:15:36 +0100

    Improve some test case description

--- src/convert.test.js
+++ src/convert.test.js
@@ -4,12 +4,12 @@ import test from 'ava';
 import Bugsy from './Bugsy';
 import convert from './convert';
 
-test('convert native Error', (t) => {
+test('convert native errors', (t) => {
   t.true(convert(new Error()) instanceof Bugsy);
   t.pass();
 });
 
-test('copy stack', (t) => {
+test('converted error contains stack', (t) => {
   const err = new Error();
 
   t.is(convert(err).stack, err.stack);
--- src/toString.test.js
+++ src/toString.test.js
@@ -5,17 +5,17 @@ import Bugsy from './Bugsy';
 import * as syslog from './syslog';
 import toString from './toString';
 
-test('native error', (t) => {
+test('stringify native error', (t) => {
   t.is(toString(new Error()), '[err] Error');
   t.pass();
 });
 
-test('severity', (t) => {
+test('stringify bugsy error with severity', (t) => {
   t.is(toString(new Bugsy({ severity: syslog.ALERT })), '[alert] Error');
   t.pass();
 });
 
-test('code', (t) => {
+test('stringify bugsy error with code', (t) => {
   t.is(toString(new Bugsy({ code: 'performance' })), '[err] performance');
   t.pass();
 });
